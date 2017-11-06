d22b372aa3ced26538a41b80e2adb29f6b51d6e9
Alex Siman <aleksandr.siman@gmail.com>
2017-11-06 06:30:39 +0200

    Add min version of jsons

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Country names with ISO 3166-1 codes (alpha-2).
+# Country names with ISO 3166-1 codes (alpha-2)
 
 Taken from: https://github.com/michaelwittig/node-i18n-iso-countries
--- get-codes.js
+++ get-codes.js
@@ -3,5 +3,5 @@ let fs = require('fs');
 let countriesEn = require('./countries-en.json').countries;
 
 let codes = Object.keys(countriesEn);
-fs.writeFileSync(`./codes.json`, JSON.stringify(codes), 'utf-8');
-fs.writeFileSync(`./codes-pretty.json`, JSON.stringify(codes, null, 2), 'utf-8');
+fs.writeFileSync(`./codes.min.json`, JSON.stringify(codes), 'utf-8');
+fs.writeFileSync(`./codes.json`, JSON.stringify(codes, null, 2), 'utf-8');
